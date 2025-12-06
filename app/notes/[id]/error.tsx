290774961d62f52e5288dc9a0f703d36c71b8b62
Olesia Konashchuk <konaschukl@gmail.com>
2025-12-06 20:29:11 +0100

    add css for error btn

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,4 +1,5 @@
 "use client";
+import css from "./NoteDetails.module.css"
 
 type Props = {
   error: Error;
@@ -8,7 +9,7 @@ type Props = {
 export default function Error({ error, reset }: Props) {
  return(<div>
     <p>Could not fetch note details. {error.message}</p>
-    <button onClick={reset}>Try again</button>
+    <button className={css.button} onClick={reset}>Try again</button>
   </div>
   )
  
